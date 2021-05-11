da9b63f70c40fbf4b89e510aff8e2ddd5d7289b2
franciscojmoyamartinez <franciscojmoyamartinez@gmail.com>
2021-05-11 16:57:54 +0200

    Change in welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,12 +42,12 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/franciscojmoyamartinez/diagnostic-manager-front" class="underline text-gray-900 dark:text-white">Diagnostic - Manager - Front</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a target="_blank" href="https://github.com/franciscojmoyamartinez/diagnostic-manager-front" class="underline text-gray-900 dark:text-white">Diagnostic - Manager - Front</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Enlace a repositorio Github dónde está alojado el proyecto de frontend para prueba técnica "Mulhacén Soft"
                                 </div>
                             </div>
                         </div>
@@ -55,12 +55,12 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/franciscojmoyamartinez/diagnostic-manager-api" class="underline text-gray-900 dark:text-white">Diagnostic - Manager - Api</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a target="_blank" href="https://github.com/franciscojmoyamartinez/diagnostic-manager-api" class="underline text-gray-900 dark:text-white">Diagnostic - Manager - Api</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                    Desde aquí podréis acceder al repositorio de backend para prueba técnicoa "Mulhacén Soft"
                                 </div>
                             </div>
                         </div>
